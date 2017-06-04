4775ab45868d67249697bfde4826ff2047f143a3
djwang <wangdejie1996@gmail.com>
2017-06-04 18:32:10 +0800

    修改了createPlan

--- TimeManagerX/AppDelegate.m
+++ TimeManagerX/AppDelegate.m
@@ -30,7 +30,7 @@
     [UINavigationBar appearance].titleTextAttributes = @{NSFontAttributeName:[UIFont boldSystemFontOfSize:19.0], NSForegroundColorAttributeName:[UIColor whiteColor]};
     [Bmob registerWithAppKey:@"3d1fc089a61e9cd1449597f5887a4aa0"];
     
-    self.window.rootViewController = [[TMHomeViewController alloc] init];
+    self.window.rootViewController = [[TMSignViewController alloc] init];
     [self.window makeKeyAndVisible];
 
     return YES;
--- TimeManagerX/TMPlanCreateViewController.m
+++ TimeManagerX/TMPlanCreateViewController.m
@@ -9,13 +9,20 @@
 #import "TMPlanCreateViewController.h"
 #import "TMPlanCreateCell.h"
 #import "TMHomeViewController.h"
-@interface TMPlanCreateViewController () <UITableViewDataSource,UITableViewDelegate,UITabBarDelegate>
+@interface TMPlanCreateViewController () <UITabBarDelegate,UITextFieldDelegate>
 
-@property (nonatomic,strong) UITableView *tableView;
 @property (nonatomic,strong) UIBarButtonItem *finishBtn;
 @property (nonatomic,strong) NSString *planName;
 @property (nonatomic,strong) NSString *planTime;
 @property (nonatomic,strong) NSString *planMotto;
+
+@property (nonatomic,strong) UILabel *planNameLabel;
+@property (nonatomic,strong) UILabel *planTimeLabel;
+@property (nonatomic,strong) UILabel *planMottoLabel;
+
+@property (nonatomic,strong) UITextField *planNameTextField;
+@property (nonatomic,strong) UITextField *planTimeTextField;
+@property (nonatomic,strong) UITextField *planMottoTextField;
 
 
 @end
@@ -26,7 +33,9 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     self.title = @"创建计划";
-    [self setupTableView];
+    [self setupPlanName];
+    [self setupPlanTime];
+    [self setupPlanMotto];
     [self setupFinishBtn];
 }
 
@@ -35,20 +44,95 @@
     // Dispose of any resources that can be recreated.
 }
 
--(void)setupTableView {
-    _tableView = [UITableView new];
-    _tableView.backgroundColor = [UIColor whiteColor];
-    _tableView.dataSource = self;
-    _tableView.delegate = self;
-    _tableView.scrollEnabled = NO;
-    _tableView.separatorStyle = NO;
-    _tableView.tableFooterView = [UIView new];
-    [self.view addSubview:_tableView];
-    _tableView.sd_layout
-    .topSpaceToView(self.view, SCREEN_WIDTH *0.2)
-    .widthIs(SCREEN_WIDTH)
-    .heightIs(SCREEN_HEIGHT *0.5);
+-(void) setupPlanName{
+    _planNameLabel = [UILabel new];
+    [self.view addSubview:_planNameLabel];
+    _planNameLabel.text = @"计划项目";
+    _planNameLabel.textAlignment = NSTextAlignmentCenter;
+    _planNameLabel.textColor = [UIColor grayColor];
+    _planNameLabel.backgroundColor = [UIColor whiteColor];
+    _planNameLabel.sd_layout
+    .leftSpaceToView(self.view, 20)
+    .topSpaceToView(self.navigationController.navigationBar, 30)
+    .widthIs(80)
+    .heightIs(40);
+    
+     _planNameTextField = [UITextField new];
+    [self.view addSubview:_planNameTextField];
+    _planNameTextField.placeholder = @"学外语，弹吉他";
+    _planNameTextField.textAlignment = NSTextAlignmentCenter;
+    UIView *paddingView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 10, 40)];
+    _planNameTextField.leftView = paddingView;
+    _planNameTextField.leftViewMode = UITextFieldViewModeAlways;
+    _planNameTextField.backgroundColor = [UIColor whiteColor];
+    _planNameTextField.sd_layout
+    .topSpaceToView(self.navigationController.navigationBar, 30)
+    .leftSpaceToView(_planNameLabel, 5)
+    .rightSpaceToView(self.view, 20)
+    .heightIs(40);
+    
+    
 }
+
+-(void) setupPlanTime{
+    _planTimeLabel = [UILabel new];
+    [self.view addSubview:_planTimeLabel];
+    _planTimeLabel.text = @"目标时间";
+    _planTimeLabel.textAlignment = NSTextAlignmentCenter;
+    _planTimeLabel.textColor = [UIColor grayColor];
+    _planTimeLabel.backgroundColor = [UIColor whiteColor];
+    _planTimeLabel.sd_layout
+    .leftSpaceToView(self.view, 20)
+    .topSpaceToView(_planNameLabel, 20)
+    .widthIs(80)
+    .heightIs(40);
+    
+    _planTimeTextField = [UITextField new];
+    [self.view addSubview:_planTimeTextField];
+    _planTimeTextField.delegate = self;
+    _planTimeTextField.placeholder = @"10000";
+    _planTimeTextField.textAlignment = NSTextAlignmentCenter;
+    _planMottoTextField.keyboardType = UIKeyboardTypeNumberPad;
+    UIView *paddingView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 10, 40)];
+    _planTimeTextField.leftView = paddingView;
+    _planTimeTextField.leftViewMode = UITextFieldViewModeAlways;
+    _planTimeTextField.backgroundColor = [UIColor whiteColor];
+    _planTimeTextField.sd_layout
+    .topSpaceToView(_planNameTextField, 20)
+    .leftSpaceToView(_planTimeLabel, 5)
+    .rightSpaceToView(self.view, 20)
+    .heightIs(40);
+}
+
+-(void) setupPlanMotto{
+    _planMottoLabel = [UILabel new];
+    [self.view addSubview:_planMottoLabel];
+    _planMottoLabel.text = @"激励语句";
+    _planMottoLabel.textAlignment = NSTextAlignmentCenter;
+    _planMottoLabel.textColor = [UIColor grayColor];
+    _planMottoLabel.backgroundColor = [UIColor whiteColor];
+    _planMottoLabel.sd_layout
+    .leftSpaceToView(self.view, 20)
+    .topSpaceToView(_planTimeLabel, 20)
+    .widthIs(80)
+    .heightIs(40);
+    
+    _planMottoTextField = [UITextField new];
+    [self.view addSubview:_planMottoTextField];
+    _planMottoTextField.placeholder = @"让优秀成为一种习惯";
+    _planMottoTextField.textAlignment = NSTextAlignmentCenter;
+    UIView *paddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 40)];
+    _planMottoTextField.leftView = paddingView;
+    _planMottoTextField.leftViewMode = UITextFieldViewModeAlways;
+    _planMottoTextField.backgroundColor = [UIColor whiteColor];
+    _planMottoTextField.sd_layout
+    .leftSpaceToView(_planMottoLabel, 5)
+    .topSpaceToView(_planTimeTextField, 20)
+    .rightSpaceToView(self.view, 20)
+    .heightIs(40);
+    
+}
+
 
 -(void)setupFinishBtn {
     _finishBtn = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"finish32"] style:UIBarButtonItemStylePlain target:self action:@selector(finishEdit:)];
@@ -58,27 +142,25 @@
 -(void)finishEdit:(id)sender {
     if([_finishBtn isEqual:sender]){
         //TODO
-        //传输数据或者保存数据到网络
-        //往task表添加一条task_name,task_from,task_to,task_location的数据
-        BmobObject *newTask = [BmobObject objectWithClassName:@"plan"];
+        BmobObject *newPlan = [BmobObject objectWithClassName:@"plan"];
         
         //将数据与用户连接
         BmobUser *bmobUser = [BmobUser currentUser];
         if(bmobUser){
-            [newTask setObject:bmobUser.username      forKey:@"user_Name"];     //user
+            [newPlan setObject:bmobUser.username      forKey:@"user_Name"];     //user
             
-            [newTask setObject:[self planName]      forKey:@"plan_Name"];
-            [newTask setObject:[self planTime]      forKey:@"plan_time"];
-            [newTask setObject:[self planMotto]     forKey:@"plan_motto"];
-            [newTask setObject:@(0)           forKey:@"plan_pain"];
+            [newPlan setObject:[self planName]      forKey:@"plan_Name"];
+            [newPlan setObject:[self planTime]      forKey:@"plan_time"];
+            [newPlan setObject:[self planMotto]     forKey:@"plan_motto"];
+            [newPlan setObject:@(0)           forKey:@"plan_pain"];
 
-            NSLog(@"%@", [self planMotto]);
-            NSLog(@"%@", [self planName]);
-            [newTask saveInBackgroundWithResultBlock:^(BOOL isSuccessful, NSError *error) {
+            NSLog(@"planMotto is %@", [self planMotto]);
+            NSLog(@"planName is%@", [self planName]);
+            [newPlan saveInBackgroundWithResultBlock:^(BOOL isSuccessful, NSError *error) {
                 if (isSuccessful) {
                     //创建成功后会返回objectId，updatedAt，createdAt等信息
                     //创建对象成功，打印对象值
-                    NSLog(@"%@",newTask);
+                    NSLog(@"%@",newPlan);
                     [KVNProgress showSuccessWithStatus:@"计划已创建"];
                     [self presentViewController:[TMHomeViewController new] animated:true completion:nil];
                 } else if (error){
@@ -92,65 +174,21 @@
         }else{
             //缓存用户对象为空时， 可打开用户注册界面…
         }
-        
     }
 }
 
--(NSString *)planName{
-    return  self.tableView.visibleCells[0].textLabel.text;
-}
-
--(NSString *)planTime{
-    return self.tableView.visibleCells[1].textLabel.text;
-}
-
--(NSString *)planMotto{
-    return self.tableView.visibleCells[2].textLabel.text;
-}
-
-#pragma mark -- tableview Delegate
-
--(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    return 1;
-}
-
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 3;
-}
-
-
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    //获取自动计算出的cell高度。SDAutoLayout方法。
-    return [self cellHeightForIndexPath:indexPath cellContentViewWidth:SCREEN_WIDTH tableView:tableView];
-}
-
-- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    BOOL classRegister = NO;
-    if(!classRegister){
-        Class myclass = [TMPlanCreateCell class];
-        [tableView registerClass:myclass forCellReuseIdentifier:@"TMPlanCreateCell"];
-        classRegister = YES;
+#pragma mark --UITextField委托方法
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    //限制_planTimeTextField只能输入数字
+    if(_planTimeTextField == textField){
+        NSCharacterSet *cs;
+        cs = [[NSCharacterSet characterSetWithCharactersInString:@"0123456789"] invertedSet];
+        NSString *filtered =
+        [[string componentsSeparatedByCharactersInSet:cs] componentsJoinedByString:@""];
+        BOOL basic = [string isEqualToString:filtered];
+        return basic;
     }
-    
-    TMPlanCreateCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TMPlanCreateCell"];
-    switch (indexPath.row) {
-        case 0:
-            cell.mTextField.placeholder = @"健身，学外语，读书";
-            cell.iconImgView.image = [UIImage imageNamed:@"list"];
-            break;
-        case 1:
-            cell.mTextField.placeholder = @"时间目标";
-            cell.iconImgView.image = [UIImage imageNamed:@"clock"];
-            break;
-        case 2:
-            cell.mTextField.placeholder = @"激励口号";
-            cell.iconImgView.image = [UIImage imageNamed:@"smile"];
-        default:
-            break;
-    }
-    return cell;
+    return NO;
 }
 
 
